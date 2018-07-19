3a31ed76c225d6e611b69d8eb9e0c8ecbc7f3e6d
Kimentii <kmalafey@gmail.com>
2018-07-19 22:47:18 +0300

    Added menu.

--- app/src/main/java/com/kimentii/virtualstorage/MainActivity.java
+++ app/src/main/java/com/kimentii/virtualstorage/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -18,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
 
         GlobalConfigurations.getInstance(getApplicationContext());
 
-        setContentView(new DrawingView(getApplicationContext()));
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View menuView = layoutInflater.inflate(R.layout.menu, linearLayout, false);
+        linearLayout.addView(menuView);
+
+        DrawingView drawingView = new DrawingView(this);
+        linearLayout.addView(drawingView);
+        setContentView(linearLayout);
     }
 }
